7b23057ba4a7e4d4e0aad1e8faf1c9c8c48adfa7
Charmull <527wlals@gmail.com>
2023-01-30 02:48:23 +0900

    store and reducer

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
+import { legacy_createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-console.log(add);
-
-let count = 0;
-
-number.innerHTML = count;
-
-const updateText = () => {
-  number.innerHTML = count;
+// data를 modify하는 함수 (reducer, modifier) - state를 modify하는 것은 이 함수 안에서만 가능
+const countModifier = (count = 0) => {
+  return count;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
+// data를 저장하는 곳
+// 처음에 countModifier(reducer)를 통해 state를 initializing
+const countStore = legacy_createStore(countModifier);
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// app의 data (즉 app이 저장하고 있는 state 중, countStore로 저장하고 있는 state를 출력)
+console.log(countStore.getState());
