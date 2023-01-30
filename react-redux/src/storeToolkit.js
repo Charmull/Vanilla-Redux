f93a32424ad5e31c0110f916302c6def05686900
Charmull <527wlals@gmail.com>
2023-01-30 15:18:16 +0900

    createReducer

--- react-redux/src/storeToolkit.js
+++ react-redux/src/storeToolkit.js
@@ -1,22 +1,21 @@
 import { legacy_createStore as createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // action creator
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      const newToDoObj = { text: action.payload, id: Date.now() };
-      return [newToDoObj, ...state];
-    case deleteToDo.type:
-      const delToDos = state.filter((toDo) => toDo.id !== action.payload);
-      return delToDos;
-    default:
-      return state;
-  }
-};
+// reducer, createReducer를 사용하면 state를 mutate하게 사용할 수 있음
+const reducer = createReducer([], {
+  // action이 addToDo일 때
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    // immutable하게도 사용 가능, 다만 어쨌든 상태 object를 return 해주어야 함
+    return state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
 
 const store = createStore(reducer);
 
